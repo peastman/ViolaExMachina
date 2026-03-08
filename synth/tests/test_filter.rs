041f6798c30dd49d1e40fc1415afb4f89716be53
Peter Eastman <peter.eastman@gmail.com>
2026-03-08 09:19:03 -0700

    Fix compilation error

--- synth/tests/test_filter.rs
+++ synth/tests/test_filter.rs
@@ -1,20 +1,20 @@
 // Copyright 2025 by Peter Eastman
 //
-// This file is part of Chorus Ex Machina.
+// This file is part of Viola Ex Machina.
 //
-// Chorus Ex Machina is free software: you can redistribute it and/or modify it under the terms
+// Viola Ex Machina is free software: you can redistribute it and/or modify it under the terms
 // of the GNU Lesser General Public License as published by the Free Software Foundation, either
 // version 2.1 of the License, or (at your option) any later version.
 //
-// Chorus Ex Machina is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// Viola Ex Machina is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
 // the GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License along with Chorus Ex Machina.
+// You should have received a copy of the GNU Lesser General Public License along with Viola Ex Machina.
 // If not, see <https://www.gnu.org/licenses/>.
 
-use chorus::filter::{Filter, LowpassFilter, HighpassFilter, BandpassFilter, ResonantFilter};
-use chorus::SAMPLE_RATE;
+use synth::filter::{Filter, LowpassFilter, HighpassFilter, BandpassFilter, ResonantFilter};
+use synth::SAMPLE_RATE;
 use std::f32::consts::PI;
 
 fn compute_response_amplitude(filter: &mut impl Filter, frequency: f32) -> f32 {
